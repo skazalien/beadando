f46256228b5bf030bfe2363d4b6ffd86b220092b
skazalien <dn.balazs99@gmail.com>
2019-05-05 23:34:18 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,2 +1,15 @@
-print("Hellobello")
-print("12; 13; 19")
+string = input("String: ") # <>={}
+num = 0
+lower = 0
+upper = 0
+other = 0
+for i in string:
+    if "a" <= i <= "z":
+        lower += 1
+    elif "A" <= i <= "Z":
+        upper += 1
+    elif "0" <= i <= "9":
+        num += 1
+    else:
+        other += 1
+print("num = {} lower = {} upper = {} other = {}".format(num, lower, upper, other))
