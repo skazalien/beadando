0b9d161409104792eb4d074bdc72783c653f731c
skazalien <dn.balazs99@gmail.com>
2019-05-13 18:29:36 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,17 +1,18 @@
-string = input("String: ") # <>={}
-num = 0
-lower = 0
-upper = 0
-other = 0
-lower_com = ["á", "é", "í","ó", "ö", "ő", "ú", "ü", "ű"]
-upper_com = ["Á", "É", "Í", "Ó", "Ö", "Ő", "Ú", "Ü", "Ű"]
-for i in string.replace(" ", ""):
-    if "a" <= i <= "z" or i in lower_com:
+import string
+
+str = input("String: ") # <>={}
+
+num,lower,upper,other = 0,0,0,0
+lower_com,upper_com = ["á", "é", "í","ó", "ö", "ő", "ú", "ü", "ű"], ["Á", "É", "Í", "Ó", "Ö", "Ő", "Ú", "Ü", "Ű"]
+for i in str.replace(" ", ""):
+    if i in string.ascii_lowercase or i in lower_com:
         lower += 1
-    elif "A" <= i <= "Z" or i in upper_com:
+    elif i in string.ascii_uppercase or i in upper_com:
         upper += 1
-    elif "0" <= i <= "9":
+    elif i.isdigit():
         num += 1
     else:
         other += 1
-print("num = {} lower = {} upper = {} other = {}".format(num, lower, upper, other))
+print("num = {}\nlower = {}\nupper = {}\nother = {}".format(num, lower, upper, other))
+
+
