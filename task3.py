967bc92ce4e776b94ee7a90705195f8eedc66271
skazalien <dn.balazs99@gmail.com>
2019-05-14 19:27:32 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,3 +1,5 @@
+import string
+
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
                    'C': '-.-.', 'D': '-..', 'E': '.',
                    'F': '..-.', 'G': '--.', 'H': '....',
@@ -45,11 +47,23 @@ def decrypt(message): #morse to msg
 message = input("Message: ").strip() # < > = {} []
 dotordash = [".","-"]
 
-if message[0].upper() in list(MORSE_CODE_DICT.keys()) and message[0] not in dotordash:
+val_list = list(MORSE_CODE_DICT.values())
+
+for i in range(0,len(message)):
+    ismorse = 0
+    if message[i] in val_list and ismorse == 0:
+        if ('-' in message[0] or '.' in message[0]) and string.ascii_letters in message is True:
+            ismorse = 0
+        else:
+            ismorse = 1
+    else:
+        ismorse = 0
+
+if ismorse == 0:
     result_in_morse = encrypt(message.upper())
     print("Original Message in Morse:", result_in_morse)
     print("Original Message:", message)
-else:
+elif ismorse == 1:
     result_in_regular = decrypt(message)
     result_in_regular=result_in_regular.capitalize()
     print("Original Message in Regular:", result_in_regular)
